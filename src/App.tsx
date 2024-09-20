ca22125a424c1f669f43b0e5877ab35a2517c0ab
Quoc Thai <lequocthai240902@gmail.com>
2024-09-20 15:06:46 +0700

    fixed container

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import NavBar from './components/NavBar';
 import Slogan from './components/Slogan';
 function App() {
     return (
-        <div className="container mx-auto h-screen overflow-y-auto overflow-x-hidden bg-gradient-to-b from-white to-pink-500 px-4 py-8 font-sora text-black dark:from-purple-900 dark:to-purple-700 dark:text-white">
+        <div className="h-screen overflow-y-auto overflow-x-hidden bg-gradient-to-b from-white to-pink-500 px-4 py-8 font-sora text-black dark:from-purple-900 dark:to-purple-700 dark:text-white">
             <NavBar />
             <Slogan />
             <Content />
